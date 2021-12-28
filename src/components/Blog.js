c3cd7b4da84216680c08c058e311f6869608d6ed
alexcarantona <info@alejandrocarantonna.es>
2021-12-28 16:01:36 +0100

    #5.7 (bloglist frontend step7) Done
    A button toggles the details of each blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    <i>{blog.title}</i> by {blog.author}
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    borderWidth: 1,
+    border: 'solid',
+    paddingTop: 10,
+    paddingLeft: 2,
+    marginBottom: 5,
+  };
+
+  const [visible, setVisible] = useState(false);
+
+  const detailsStyle = visible
+    ? {display: ''}
+    : {display: 'none'}
+
+  return (
+  <div style={blogStyle}>
+    <span>
+      <i>{blog.title}</i> by {blog.author}
+      <button onClick={() => setVisible(!visible)}>
+      {visible ? 'Hide' : 'Details'}
+      </button>
+    </span>
+    <section style={detailsStyle}>
+      <p>{blog.url}</p>
+      <p>Likes: {blog.likes}<button>Like it!</button></p>
+      <p>Added by: {blog.user.username}</p>
+    </section>
   </div>
 )
+};
 
 export default Blog
