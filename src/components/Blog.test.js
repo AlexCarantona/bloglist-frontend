4f7edc83fdd84f6a6601d5c698b9bc864f8c2938
alexcarantona <info@alejandrocarantonna.es>
2021-12-29 00:23:58 +0100

    #5.14 & 5.15 (bloglist tests steps 2 & 3) Done
    Tested display of hidden content and two calls to the like button

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
-test("Renders blog info", () => {
+
+describe('Testing Blog component...', () => {
+
+  let component;
+  const updateHandler = jest.fn();
   const blogContent = {
     title: 'A blog',
     author : 'An author',
@@ -12,17 +16,47 @@ test("Renders blog info", () => {
     },
     url: 'url.com',
     likes: 12
-  }
+  };
 
-  const component = render(<Blog blog={blogContent} />);
-  const container = component.container.querySelector('.blogInfo');
-  const header = component.container.querySelector('.blogHeader');
-  const details = component.container.querySelector('.blogDetails')
+  beforeEach(() => {
+    component = render(<Blog
+      blog={blogContent}
+      updateHandler={updateHandler} />);
+  });
 
-  expect(container).toHaveTextContent(
-    "A blog by An author"
-  );
-  expect(details).toHaveStyle(
-    'display: none'
-  );
+  test("Renders blog info (only header)", () => {
+    const header = component.container.querySelector('.blogHeader');
+    expect(header).toHaveTextContent(
+      "A blog by An author"
+    );
+    const button = component.container.querySelector('button');
+    expect(button).toHaveTextContent(
+      "Details"
+    );
+    const details = component.container.querySelector('.blogDetails');
+    expect(details).toHaveStyle(
+      'display: none'
+    );
+  });
+
+  test("Renders blog details when 'details' is clicked.", () => {
+    const button = component.container.querySelector('button');
+    fireEvent.click(button);
+    expect(button).toHaveTextContent(
+      "Hide"
+    );
+    const details = component.container.querySelector('.blogDetails');
+    expect(details).not.toHaveStyle(
+      'display: hide'
+    );
+  });
+
+  test("Clicking 'like' button calls the like handler", () => {
+    const likeButton = component.getByText('Like it!');
+    fireEvent.click(likeButton);
+    fireEvent.click(likeButton);
+
+    expect(updateHandler.mock.calls).toHaveLength(2);
+    expect(updateHandler.mock.calls[0][0]).toBe(blogContent);
+  })
 })
